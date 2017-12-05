6319879f646044320bcfb2cc7f0a82242dc0437a
Jason Mayberry <mayberry.pdr@gmail.com>
2017-12-05 09:31:58 -0600

    Create Update-JS-Bookmarks.js

---
+++ Update-JS-Bookmarks.js
@@ -0,0 +1,64 @@
+
+
+var bookmarks = [
+
+`http://javascript.info/
+http://apprize.info/javascript/index.html
+https://cssauthor.com/css-reference/
+https://www.w3.org/community/webed/wiki/HTML
+https://www.w3.org/community/webed/wiki/CSS
+https://www.sitepoint.com/javascript/
+https://stackoverflow.com/questions/823718/what-are-some-of-the-best-reference-sites-for-html-and-javascript-programming
+http://blog.teamtreehouse.com/mastering-developer-tools-console
+https://tympanus.net/codrops/
+https://www.quackit.com/
+http://www.learn-js.org/en/Manipulating_Arrays`,
+
+
+
+`https://www.youtube.com/watch?v=ifig4xSp0kA 
+https://www.google.com/search?q=b/g+planeswalker&rlz=1CASMAI_enUS771US771&tbm=shop&source=lnms&sa=X&ved=0ahUKEwiXruO5xtvXAhWkx4MKHSZkArIQ_AUICigB&biw=1366&bih=629&dpr=1#spd=0
+https://www.youtube.com/watch?v=10LeYkpSr2E
+http://gatherer.wizards.com/Pages/Search/Default.aspx?page=1&color=+[B]+[G]&sort=rarity+
+http://gatherer.wizards.com/Pages/Search/Default.aspx?output=spoiler&method=visual&sort=color+&action=advanced&block=+%5b%22Zendikar+Block%22%5d&subtype=+%5b%22Eldrazi%22%5d
+https://www.google.com/search?q=meren+of+clan+nel+toth&rlz=1CASMAI_enUS771US771&source=lnms&tbm=shop&sa=X&ved=0ahUKEwjGxbnu7dfXAhVl_4MKHTVJCGwQ_AUICigB&biw=1366&bih=629#spd=0
+https://www.google.com/search?q=sapling+of+colfenor&rlz=1CASMAI_enUS771US771&source=lnms&tbm=shop&sa=X&ved=0ahUKEwjTm8Gp7tfXAhUm4YMKHTYLDKoQ_AUICygC&biw=1366&bih=629#spd=0
+https://www.google.com/search?q=mtg+ulamog&rlz=1CASMAI_enUS771US771&source=lnms&tbm=shop&sa=X&ved=0ahUKEwiOno-nhNjXAhXo6IMKHQajCPMQ_AUICigB&biw=1366&bih=629#spd=0
+https://www.google.com/search?q=kozilek+mtg&rlz=1CASMAI_enUS771US771&source=lnms&tbm=shop&sa=X&ved=0ahUKEwjru4GzhdjXAhXr4IMKHfoZCWIQ_AUICigB&biw=1366&bih=629#spd=0
+https://www.amazon.com/Huge-4000-Magic-Gathering-Collection/dp/B00SVETK9O/ref=pd_sbs_21_6?_encoding=UTF8&pd_rd_i=B00SVETK9O&pd_rd_r=TFFGB6SERXDH748BYSFE&pd_rd_w=ANeR6&pd_rd_wg=D2yeO&psc=1&refRID=TFFGB6SERXDH748BYSFE`,
+
+
+
+`https://github.com/maqetta/maqetta/releases
+https://chrome.google.com/webstore/detail/chrome-dev-editor/pnoffddplpippgcfjdhbmhkofpnaalpg/reviews
+https://opensource.com/alternatives/dreamweaver
+https://chrome.google.com/webstore/detail/polymer-devtools-extensio/mmpfaamodhhlbadloaibpocmcomledcg?hl=en
+https://github.com/GoogleChrome/chromedeveditor
+https://www.google.com/search?safe=active&rlz=1C5CHFA_enUS712US712&biw=1309&bih=690&q=open+source+html+editor&oq=open+source+html&gs_l=psy-ab.1.0.0l4.840543.853310.0.856682.17.17.0.0.0.0.377.3172.0j10j3j3.16.0....0...1.1.64.psy-ab..1.16.3171.6..35i39k1j0i67k1j0i20k1.4rPXRrxm408
+http://maqetta.org/
+http://www.aptana.com/products/studio3/download.html
+https://www.w3schools.com/js/tryit.asp?filename=tryjs_whereto
+https://atom.io/
+http://codemirror.net/
+https://www.thefreecountry.com/webmaster/htmleditors.shtml
+https://c9.io/pricing
+https://coderwall.com/p/lhsrcq/one-line-browser-notepad
+http://codegists.com/search/ace-editor-js-cdn-unavailable/2`,
+
+];
+
+
+
+
+var newObjArray = [
+
+new Bookmark(bookmarks[0], 'Reference Sites'),
+new Bookmark(bookmarks[1], 'Jakes Wish List'),
+new Bookmark(bookmarks[2], 'HTML Editors'),
+
+];
+
+
+
+
+
